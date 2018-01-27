92ec31cd113b6fe9589053bbf2996bce4ee44925
dracconi <kacmalina@gmail.com>
2018-01-27 14:24:05 +0100

    some structs

---
+++ handler/message.go
@@ -0,0 +1,6 @@
+package handler
+
+// Message is just what is is
+type Message struct {
+	messenger Messenger
+}
---
+++ handler/messenger.go
@@ -0,0 +1,6 @@
+package handler
+
+// Messenger is the Message sender (User)
+type Messenger struct {
+	username string
+}
